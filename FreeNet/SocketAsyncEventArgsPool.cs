c1eb6e473e0dc4a875447fa9b5504ddc2bd007b3
HulkSTD <hulk1257@gmail.com>
2020-06-25 15:40:55 +0900

    서버 SDK DLL 소스

---
+++ FreeNet/SocketAsyncEventArgsPool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace FreeNet
+{
+    class SocketAsyncEventArgsPool
+    {
+        Stack<SocketAsyncEventArgs> m_pool;
+
+        public SocketAsyncEventArgsPool(int capacity)
+        {
+            m_pool = new Stack<SocketAsyncEventArgs>(capacity);
+        }
+
+        public void Push(SocketAsyncEventArgs item)
+        {
+            if(item == null) { throw new ArgumentNullException("Items added to a SocketAsyncEventArgsPool cannot be null"); }
+
+            lock (m_pool)
+            {
+                m_pool.Push(item);
+            }
+        }
+
+        public SocketAsyncEventArgs Pop()
+        {
+            lock(m_pool)
+            {
+                return m_pool.Pop();
+            }
+        }
+
+        public int Count
+        {
+            get { return m_pool.Count; }
+        }
+
+    }
+}
